60ed2cc7b3e211dfc9b8030d7d3589c2f2e9cfdc
Tanner <Branham>
2021-03-03 02:39:50 -0500

    add procfile

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const { render } = require('ejs');
 
+// port setup
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
 //express app
 const app = express();
 
 // connect to mongodb
 const dbURI = process.env.MON;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(port))
     .catch((err) => console.log(err));
 
 // register view engine
